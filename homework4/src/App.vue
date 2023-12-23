ee5ed6eb4a1fe77b599d2d622b5a6226bfbd0bd4
The_Valkyrie <110331611+Kirieshek@users.noreply.github.com>
2023-12-23 13:56:13 +0300

    Add files via upload

---
+++ homework4/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <input v-model="input1" placeholder="Введите текст" />
+    <input v-model="input2" placeholder="Введите текст" />
+    <button @click="swapText">Обменять тексты</button>
+  </div>
+
+  <br>
+  <br>
+  <br>
+
+  <div>
+    <input type="text" v-model="fio" placeholder="ФИО">
+    <button @click="splitFio">Получить ФИО</button>
+    <p>{{ lastName }}</p>
+    <p>{{ firstName }}</p>
+    <p>{{ middleName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+      swapText() {
+        const temp = this.input1;
+        this.input1 = this.input2;
+        this.input2 = temp;
+      },
+      splitFio() {
+        const fioArray = this.fio.split(" ");
+        this.lastName = fioArray[0];
+        this.firstName = fioArray[1];
+        this.middleName = fioArray[2];
+      } 
+    },
+  
+  computed: {
+
+  },
+  data() {
+    return {
+      input1: '',
+      input2: '',
+      fio: "",
+      lastName: "",
+      firstName: "",
+      middleName: ""
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
